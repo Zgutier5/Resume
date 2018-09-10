ebc86bbc22c903dfe155e4ce6426485ed7512608
Ezequiel Gutierrez <zgutier5@gmail.com>
2018-09-10 18:05:54 -0500

    first draft

---
+++ src/componets/Education.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+
+class Education extends Component {
+  render() {
+    return (
+     <div>
+        <h2>Education and Honors</h2>
+        <p>
+            <li>BBA of Business Administration	May 2012</li>
+            <li>St. Edward’s University, Austin, TX</li>
+            <li>College Assistance Migrant Program Academic Scholarship and Munday Endowed Scholarship</li>
+        </p>
+       
+     </div>
+    );
+  }
+}
+
+export default Education;
